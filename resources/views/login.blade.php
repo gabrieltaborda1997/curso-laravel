bf77d695fe65e781399b7ae4331dfa160eac4c5c
Gabriel Taborda Dias <gabrieltabordadias@hotmail.com>
2020-04-25 17:41:50 -0300

    add views

---
+++ resources/views/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konnect Sistem</title>
+    <link rel="stylesheet" href="_css/style.css">
+</head>
+<body>
+    <form class='form' action="../logar.php" method="POST">
+        <div class="card">
+            <div class="card-top">
+                <img class="img-login" src="_img/user.png" alt="">
+                <h2 class="tittle">Painel de Acesso</h2>
+                <p>Gerenciar seu Negócio</p>
+            </div>
+            <div class="card-group">
+                <label>Email</label>
+                <input type="email" name="email" id="email" placeholder=" Digite seu email" required>
+            </div>
+            <div class="card-group">
+                <label>Senha</label>
+                <input type="password" name="senha" id="senha" placeholder=" Digite sua senha" required>
+            </div>
+            <div class="card-group">
+                <label><input type="checkbox" name="check" id="check">Lembre-me</label>
+            </div>
+            <div class="card-group btn">
+                <button type="submit">Acessar</button>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function(){
+    return view('login');
+});
